fed6b4832aa0d3b3a16c473c2fdae74a5e713c9b
Sirajul <shirajulislamparvez@gmail.com>
2023-03-05 14:57:37 +0600

    Adding 'Grid View'

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,30 @@ class HomeActivity extends StatelessWidget{
      {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Shahidul"},
      {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Fahad"},
      {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Mahibul"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Sirajul"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Ebrahim"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Aman"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Sadi"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Yousuf"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Sudipto"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Rakib"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Rajesh"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Abdullah"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Shahidul"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Fahad"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Mahibul"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Sirajul"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Ebrahim"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Aman"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Sadi"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Yousuf"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Sudipto"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Rakib"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Rajesh"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Abdullah"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Shahidul"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Fahad"},
+     {"img":"https://cdn.rabbil.com/photos/images/2022/12/27/flutterS.png","title":"Flutter Mahibul"},
 
    ];
    
@@ -43,10 +67,11 @@ class HomeActivity extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dynamic List View"),
+        title: Text("Dynamic List / Grid View"),
       ),
 
-        body: ListView.builder(
+        //ListViewBuilder
+      /*  body: ListView.builder(
         itemCount: MyItems.length,
         itemBuilder: (context,index){
           return GestureDetector(
@@ -59,7 +84,28 @@ class HomeActivity extends StatelessWidget{
             ),
           );
         },
-      )
+      )*/
+
+      //GirdView Buider
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 0,
+            childAspectRatio: 1.2
+          ) ,
+          itemCount: MyItems.length,
+          itemBuilder: (context,index){
+            return GestureDetector(
+              onLongPress: (){mySnackBar(context, MyItems[index]['title']);},
+              child: Container(
+                margin: EdgeInsets.all(05),
+                width: double.infinity,
+                height: 250,
+                child: Image.network(MyItems[index]['img']!,fit: BoxFit.fill,),
+              ),
+            );
+          },
+        )
     );
   }
 }
